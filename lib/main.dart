79269a24443ea4c713ecb60685bb9a4b6818827b
kla gueu <klagueu@gmail.com>
2023-11-09 16:12:21 +0000

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -53,11 +53,11 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              'Finger data: $_data',
             ),
             Text(
-              '$_data',
+              '\n$_data',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
